7208cd2f6c1b63f4bcc5890545ce0669f8c0749b
BHAVANA123-SRI <106794973+BHAVANA123-SRI@users.noreply.github.com>
2023-12-14 12:45:28 +0530

    Update Home.py

--- Project_1/Home.py
+++ Project_1/Home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image, ImageEnhance, ImageFilter
+import io
+from base64 import b64encode
 
 st.markdown("<h1 style='text-align: center; font-size:40px; color: black;'>Image operations📸</h1>", unsafe_allow_html=True)
-
-
 img_path = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 operation = st.selectbox("Select Operation", ["Enhance", "Filter", "Resize", "Crop"])
 
@@ -14,25 +14,17 @@ if img_path is not None:
     img = Image.open(img_path)
 
     if operation == "Enhance":
-        enhancement_factor = 2.5
+        enhancement_factor = st.slider("Select enhancement factor", 1.0, 3.0, 2.0)
         enhancer = ImageEnhance.Color(img)
         enhanced_image = enhancer.enhance(enhancement_factor)
         st.image(enhanced_image, caption="Enhanced Image", use_column_width=True)
         final_image = enhanced_image  # Set final_image after the operation
 
     elif operation == "Filter":
-        blur_image = img.filter(ImageFilter.BLUR)
-        contour_image = img.filter(ImageFilter.CONTOUR)
-        find_edges_image = img.filter(ImageFilter.FIND_EDGES)
-        emboss_enhance_image = img.filter(ImageFilter.EMBOSS)
-
-        col1, col2 = st.columns(2)
-        col1.image(blur_image, caption="Blur Filtered Image", use_column_width=True)
-        col1.image(contour_image, caption="Contour Filtered Image", use_column_width=True)
-        col2.image(find_edges_image, caption="Find Edges Filtered Image", use_column_width=True)
-        col2.image(emboss_enhance_image, caption="Emboss Filtered Image", use_column_width=True)
-
-        final_image = emboss_enhance_image  # Set final_image after the operations
+        selected_filter = st.selectbox("Select Filter", ["BLUR", "CONTOUR", "FIND_EDGES", "EMBOSS"])
+        filter_image = img.filter(getattr(ImageFilter, selected_filter))
+        st.image(filter_image, caption=f"{selected_filter} Filtered Image", use_column_width=True)
+        final_image = filter_image  # Set final_image after the operation
 
     elif operation == "Resize":
         new_width = st.text_input("Enter new width")
@@ -44,7 +36,6 @@ if img_path is not None:
 
             resized_image = img.resize((new_width, new_height))
             st.image(resized_image, caption="Resized Image", use_column_width=True)
-
             final_image = resized_image  # Set final_image after the operation
 
     elif operation == "Crop":
@@ -55,9 +46,15 @@ if img_path is not None:
 
         cropped_image = img.crop((left, top, right, bottom))
         st.image(cropped_image, caption="Cropped Image", use_column_width=True)
-
         final_image = cropped_image  # Set final_image after the operation
 
-# Download button for the final processed image
-if final_image is not None:
-    download_button = st.download_button(label="Download Final Processed Image", data=final_image.tobytes(), file_name="final_processed_image.png", key="download_final_image")
+    # Download button for the final processed image
+    if final_image is not None:
+        download_button_label = f"Download {operation} Image"
+        download_button_key = f"download_{operation.lower()}_image"
+
+        final_image_bytes = io.BytesIO()
+        final_image.save(final_image_bytes, format='PNG')
+        final_image_data = final_image_bytes.getvalue()
+
+        st.markdown(f'<a href="data:file/png;base64,{b64encode(final_image_data).decode()}" download="{operation.lower()}_image.png">{download_button_label}</a>', unsafe_allow_html=True)
